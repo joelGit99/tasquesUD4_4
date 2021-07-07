672e6889f2e7671488a8c0458f5a3ca2ee60978f
joelGit99 <fernandez.rodriguez.joel.21@gmail.com>
2021-07-07 11:18:00 +0200

    Tarea 4 de la Unidad 4

---
+++ TasquesUD4_4/src/TasquesUD4_4.java
@@ -0,0 +1,19 @@
+
+public class TasquesUD4_4 {
+	/**
+	 * Programa que realiza diferentes operaciones
+	 * @author Joel
+	 */
+	public static void main(String[] args) {
+		int n = 20;;
+		System.out.println("Valor inicial de N = " + n);
+		
+		n = n + 77;
+		System.out.println("N" + " + " + 77 + " = " + n);
+		n = n - 3;
+		System.out.println("N" + " - " + 3 + " = " + n);
+		n = n * 2;
+		System.out.println("N" + " * " + 2 + " = " + n);
+	}
+
+}
